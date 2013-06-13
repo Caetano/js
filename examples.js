fcb2d13395a122e57e7a16f7c40a98137dc7cce6
Lucas Nogueira Munhoz <ln.munhoz@gmail.com>
2013-06-13 10:24:18 -0300

    First commit.

---
+++ examples.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    //Find checked checkboxes in a table and get his name attribute.
+    $('#btn').click(function(){
+        var cbNames[];
+        $('#table td').find('input:checkbox:checked').each(function (i) {
+          cbNames[i] = ($(this).attr('name'));
+        });
+    });
+
+    //Send an array to Controller in ASP.NET MVC
+    $.ajaxSettings.traditional = true; // <-- Important
+    $('#btn').click(function () {
+        var array = [];
+        var url = '/Controller/Action';
+        $.post(url, { array : array });
+        });
+    });
+
+});
